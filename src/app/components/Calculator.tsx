d1c0d22d29807e493859ac3d31e03d4ceb63ae20
Mitiku <se.mitiku.yohannes@gmail.com>
2019-02-17 18:23:39 +0300

    working for some operations

--- src/app/components/Calculator.tsx
+++ src/app/components/Calculator.tsx
@@ -1,13 +1,20 @@
 import * as React from 'react';
 import * as ReactDOM from "react-dom";
 import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/js/bootstrap.js';
+// import 'bootstrap/dist/js/bootstrap.js';
 import "./calculator.css";
 
 class CalculatorBoard extends React.Component<any, any>{
-    renderSquare(txt:string) {
-        return(
-            < Square txt={txt} />);
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            onClick: props.onClick
+            
+        }
+    }
+    renderSquare(txt: string) {
+        return (
+            <Square txt={txt} onClick={(e: Event) => this.state.onClick(e, txt)} />);
     }
     render() {
         return (
@@ -46,27 +53,90 @@ class CalculatorBoard extends React.Component<any, any>{
             </div>
         );
     }
+
+
 }
 
 
 export class Calculator extends React.Component<any, any>{
-    
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            equationValue: props.equationValue,
+            currentOperator: null,
+            prevNumber:null
+        };
+        this.handleClick = this.handleClick.bind(this);
+    }
     render() {
         return (
-      
-            <>
-            <div className="equation-area row">
-                {this.props.equationValue}
+            <div className="main">
+                <div className="equation-area row">
+                    {this.state.equationValue}
+                </div>
+                <CalculatorBoard onClick={this.handleClick} />
             </div>
-            <CalculatorBoard/>
-            </>
-            
+
+
         );
     }
-    
+    handleClick(e: Event, txt: string) {
+        let txt_numbers = Array.from({ length: 10 }, (x, i) => i).map(function (item) {
+            return item.toString();
+        });
+        let percent = "%";
+        let operators = ["÷", "X", "-", "+"]
+        if (txt === "AC") {
+            const equationValue = "0";
+            this.setState({ equationValue: equationValue });
+
+        } else if (txt == "+/-") {
+            console.log("pm");
+        } else if (txt == "%") {
+            console.log("percent");
+        } else if (txt === "=") {
+            if (this.state.currentOperator === "+") {
+                let sol = Number.parseFloat(this.state.prevNumber)+ Number.parseFloat(this.state.equationValue);
+                this.setState({equationValue:sol})
+            } else if (this.state.currentOperator === "-") {
+                let sol = Number.parseFloat(this.state.prevNumber) - Number.parseFloat(this.state.equationValue);
+                this.setState({ equationValue: sol })
+            }else if (this.state.currentOperator === "X") {
+                let sol = Number.parseFloat(this.state.prevNumber) * Number.parseFloat(this.state.equationValue);
+                this.setState({ equationValue: sol })
+            
+            } else if (this.state.currentOperator === "÷") {
+                let sol = Number.parseFloat(this.state.prevNumber) / Number.parseFloat(this.state.equationValue);
+                this.setState({ equationValue: sol })
+            }
+        }
+
+        else if (operators.indexOf(txt) > -1) {
+            this.setState({
+                currentOperator: txt,
+                prevNumber: this.state.equationValue,
+                equationValue:"0"
+            })
+        }
+        else if (txt_numbers.indexOf(txt) > -1) {
+
+            let equationValue = this.state.equationValue;
+            if (equationValue === "0" || equationValue==="" || equationValue==null) {
+                equationValue = txt;
+            } else {
+                equationValue += txt;
+            }
+            this.setState({ equationValue: equationValue });
+            
+        }
+
+        else {
+            console.log("else");
+        }
+    }
 }
 class EquationArea extends React.Component<any, any>{
-    
+
     render() {
         return (
             <div>
@@ -76,12 +146,16 @@ class EquationArea extends React.Component<any, any>{
     }
 }
 class Square extends React.Component<any, any>{
-    
+
     render() {
         return (
-            <button className={this.props.txt == "0" ?"button square col-lg-6 h-20":"button square col-lg-3 h-100"}>
+            <button
+                onClick={() => this.props.onClick()}
+                className={this.props.txt == "0" ? "button square col-lg-6 h-20" : "button square col-lg-3 h-100"}
+            >
                 {this.props.txt}
             </button>
         );
     }
+
 }
